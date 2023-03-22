51d5d1be43ac5aee6548bb1814b1479022bc5dd4
HarshPatel5940 <harshnpatel9@gmail.com>
2023-03-22 13:51:40 +0530

    Updated func -> status codes

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -3,32 +3,45 @@ import { petSchema, ownerSchema } from "../schema/yup-schemas";
 import { db } from "./mongo";
 
 async function validateUuid(
-    col: "pets" | "owners"
-): Promise<string | undefined> {
-    let uuid = nanoid();
-
+    col: "pets" | "owners",
+    uuid: string
+): Promise<Number> {
     const result = await db.collection(col).find({ uuid: uuid }).toArray();
-
     if (result.length === 0) {
-        return uuid;
+        console.log("> 302 :: uuid Found");
+        return 302;
     } else {
-        console.log("UUID : REGENERATING A NEW ONE one");
-        await validateUuid(col);
+        console.log("> 302 :: uuid Not Found");
+        return 404;
     }
 }
 
-export async function validateOwner(owner: any): Promise<boolean> {
+export async function validateOwner(owner: any): Promise<Number> {
     if (await ownerSchema.isValid(owner)) {
-        return true;
+        if ((await validateUuid("owners", owner.uuid)) === 302) {
+            console.log("> 200 :: Owner Validation OK | Found");
+            return 200;
+        } else {
+            console.log("> 404 :: Owner Validation OK | NOT FOUND");
+            return 404;
+        }
     } else {
-        return false;
+        console.log("> 400 :: Owner Validation Failed ");
+        return 400;
     }
 }
 
-export async function validatePet(pet: any): Promise<boolean> {
+export async function validatePet(pet: any): Promise<Number> {
     if (await petSchema.isValid(pet)) {
-        return true;
+        if ((await validateUuid("pets", pet.uuid)) === 302) {
+            console.log("> 200 :: Pet Validation OK | Found");
+            return 200;
+        } else {
+            console.log("> 404 :: Pet Validation OK | NOT FOUND");
+            return 404;
+        }
     } else {
-        return false;
+        console.log("> 400 :: Pet Validation Failed ");
+        return 400;
     }
 }
