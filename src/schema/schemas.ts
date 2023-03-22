14cd612a295da0dd1913681f61a665324a88bcdc
HarshPatel5940 <harshnpatel9@gmail.com>
2023-03-22 11:18:20 +0530

    Added Schemas

--- src/schema/schemas.ts
+++ src/schema/schemas.ts
@@ -1,1 +1,18 @@
 // id, name of the pet, name of the owner, age, type and gender.
+import yup from "yup";
+
+export const petSchema = yup.object().shape({
+    uuid: yup.string().required(),
+    OwnerID: yup.string().required(),
+    petName: yup.string().required(),
+    petAge: yup.number().required(),
+    petType: yup.string().required(),
+    petGender: yup.string().required(),
+});
+
+export const ownerSchema = yup.object().shape({
+    uuid: yup.string().required(),
+    ownerName: yup.string().required(),
+    ownerAge: yup.number().required(),
+    OwnerContact: yup.string(), // Not Required!!
+});
