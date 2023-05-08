660a1ea536d3203b4d0315612415d2af5259a36f
Harsh Patel <harshnpatel9@gmail.com>
2023-05-08 22:34:02 +0530

    Fixed Routers TODO: Testing Pending

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,19 @@ export const app = express();
 app.use(express.json());
 
 app.get("/", function (req, res) {
-    res.status(200).send(
-        "Hello World! Thanks for using Pet-Store-API, Fell free to checkout other ENDPOINTS"
-    );
+    res.status(200).send("Hello World! Thanks for using Pet-Store-API, Fell free to checkout other ENDPOINTS");
 });
 
 app.get("/api/CheckConnection", async (req: Request, res: Response) => {
     res.sendStatus(await CheckConnection());
 });
+
+// importing routes for owners and pets file
+import owners from "./routes/owners";
+import pets from "./routes/pets";
+
+app.use("/api/owners", owners);
+app.use("/api/pets", pets);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
